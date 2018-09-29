63176bd6411dda8696d91b9d0e86e2b95f19cf07
Dave Pitts <dgapitss@gmail.com>
2018-09-29 22:15:55 +0200

    append useful alias script to .bashrc for postgres user

--- provision.sh
+++ provision.sh
@@ -3,6 +3,7 @@ if [ ! -f /home/vagrant/already-installed-flag ]
 then
   echo "ADD EXTRA ALIAS VIA .bashrc"
   cat /vagrant/bashrc.append.txt >> /home/vagrant/.bashrc
+  cat /vagrant/bashrc.append.txt >> /var/lib/pgsql/.bashrc
   #echo "GENERAL YUM UPDATE"
   #yum -y update
   #echo "INSTALL GIT"
