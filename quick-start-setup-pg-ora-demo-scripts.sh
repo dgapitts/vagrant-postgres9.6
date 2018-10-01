836ea940dac65d6ba9422f9d7911831d2d289892
Dave Pitts <dgapitss@gmail.com>
2018-10-01 22:29:25 +0200

    fix quick setup bug:  psql Permission denied

--- quick-start-setup-pg-ora-demo-scripts.sh
+++ quick-start-setup-pg-ora-demo-scripts.sh
@@ -1,3 +1,3 @@
 sudo su -c "cd ~;pwd;git clone https://github.com/dgapitts/pg-ora-demo-scripts.git;ls -l pg-ora-demo-scripts" -s /bin/sh postgres
 cp /vagrant/quick-start-setup-pg-ora-demo-scripts.sql /tmp/quick-start-setup-pg-ora-demo-scripts.sql
-sudo su postgres psql postgres -f /tmp/quick-start-setup-pg-ora-demo-scripts.sql
+sudo su -c "psql postgres -f /tmp/quick-start-setup-pg-ora-demo-scripts.sql" -s /bin/sh postgres
